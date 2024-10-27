082eaa7809bd81a659b2edfa0ebb5532f89b8b1e
David Fox <dsf@seereason.com>
2024-10-27 10:12:47 -0700

    Check in Sort.hs

---
+++ SeeReason/Errors/Sort.hs
@@ -0,0 +1,105 @@
+{-# LANGUAGE GADTs, DataKinds, KindSignatures, TypeOperators, TypeFamilies,
+             MultiParamTypeClasses, FlexibleInstances, PolyKinds,
+             FlexibleContexts, UndecidableInstances, ConstraintKinds,
+             ScopedTypeVariables, TypeInType #-}
+module SeeReason.Errors.Sort (Nub, Sort, TypeId, AsSet) where
+
+import GHC.TypeLits
+-- import Data.Proxy (Proxy(Proxy))
+import Data.Type.Bool
+import Data.Type.Equality
+
+-- :kind! Nub (Sort '[Int, Char, Int, Double, [Float]])
+-- Nub (Sort '[Int, Char, Int, Double, [Float]]) :: [*]
+-- = '[Char, Double, Int, [Float]]
+
+-- | Uniquify a sorted list.
+type family Nub xs where
+  Nub '[] = '[]
+  Nub '[x] = '[x]
+  Nub (x ': x ': ys) = Nub (x ': ys)
+  Nub (x ': y ': ys) = x ': Nub (y ': ys)
+
+-- | List append (essentially set disjoint union)
+type family (:++) (x :: [k]) (y :: [k]) :: [k] where
+            '[]       :++ xs = xs
+            (x ': xs) :++ ys = x ': (xs :++ ys)
+
+-- | Type-level quick sort for normalising the representation of sets
+type family Sort (xs :: [k]) :: [k] where
+            Sort '[]       = '[]
+            Sort (x ': xs) = ((Sort (Filter 'FMin x xs)) :++ '[x]) :++ (Sort (Filter 'FMax x xs))
+
+data Flag = FMin | FMax
+
+-- | Open-family for the ordering operation in the sort
+
+-- | Compute a symbol which uniquely identifies a type.  This is used
+-- to avoid having to write 'Cmp' instances for every pair of types.
+-- Add instances for all the types you want to use.  You are
+-- responsible for ensuring that they are all distinct and unique.
+type family TypeId (a :: *) :: Symbol
+type instance TypeId Char = "Char"
+type instance TypeId Double = "Double"
+type instance TypeId Int = "Int"
+type instance TypeId Float = "Float"
+type instance TypeId [a] = AppendSymbol "[" (AppendSymbol (TypeId a) "]")
+
+type family Cmp (a :: k) (b :: k) :: Ordering
+type instance Cmp (k :: Symbol) (k' :: Symbol) = CmpSymbol k k'
+type instance Cmp (a :: *) (b :: *) = CmpSymbol (TypeId a) (TypeId b)
+
+type family Filter (f :: Flag) (p :: k) (xs :: [k]) :: [k] where
+            Filter f p '[]       = '[]
+            Filter 'FMin p (x ': xs) = If (Cmp x p == 'LT) (x ': (Filter 'FMin p xs)) (Filter 'FMin p xs)
+            Filter 'FMax p (x ': xs) = If (Cmp x p == 'GT || Cmp x p == 'EQ) (x ': (Filter 'FMax p xs)) (Filter 'FMax p xs)
+
+type family DeleteFromList (e :: elem) (list :: [elem]) where
+    DeleteFromList elem '[] = '[]
+    DeleteFromList elem (x ': xs) = If (Cmp elem x == 'EQ)
+                                       xs
+                                       (x ': DeleteFromList elem xs)
+
+type AsSet s = Nub (Sort s)
+
+{-
+type family Delete elem set where
+    Delete elem (Set xs) = Set (DeleteFromList elem xs)
+
+-- | Value-level quick sort that respects the type-level ordering
+class Sortable xs where
+    quicksort :: Set xs -> Set (Sort xs)
+
+instance Sortable '[] where
+    quicksort Empty = Empty
+
+instance (Sortable (Filter FMin p xs),
+          Sortable (Filter FMax p xs), FilterV FMin p xs, FilterV FMax p xs) => Sortable (p ': xs) where
+    quicksort (Ext p xs) = ((quicksort (less p xs)) `append` (Ext p Empty)) `append` (quicksort (more p xs))
+                           where less = filterV (Proxy::(Proxy FMin))
+                                 more = filterV (Proxy::(Proxy FMax))
+
+-- | Filter out the elements less-than or greater-than-or-equal to the pivot
+class FilterV (f::Flag) p xs where
+    filterV :: Proxy f -> p -> Set xs -> Set (Filter f p xs)
+
+instance FilterV f p '[] where
+    filterV _ _ Empty      = Empty
+
+instance (Conder ((Cmp x p) == LT), FilterV FMin p xs) => FilterV FMin p (x ': xs) where
+    filterV f@Proxy p (Ext x xs) = cond (Proxy::(Proxy ((Cmp x p) == LT)))
+                                        (Ext x (filterV f p xs)) (filterV f p xs)
+
+instance (Conder (((Cmp x p) == GT) || ((Cmp x p) == EQ)), FilterV FMax p xs) => FilterV FMax p (x ': xs) where
+    filterV f@Proxy p (Ext x xs) = cond (Proxy::(Proxy (((Cmp x p) == GT) || ((Cmp x p) == EQ))))
+                                        (Ext x (filterV f p xs)) (filterV f p xs)
+
+class Conder g where
+    cond :: Proxy g -> Set s -> Set t -> Set (If g s t)
+
+instance Conder True where
+    cond _ s _ = s
+
+instance Conder False where
+    cond _ _ t = t
+-}
