07e334e82b0767f2f97deee34f20be71694bc6dd
David Fox <dsf@seereason.com>
2024-10-31 07:18:47 -0700

    Restore the friendlier type not found message

--- SeeReason/Errors/Types.hs
+++ SeeReason/Errors/Types.hs
@@ -36,30 +36,30 @@ import Data.SafeCopy
 import qualified Data.Serialize as S (Serialize(get, put), getWord8, Put, PutM, Get)
 import Data.Typeable (Typeable, typeOf)
 import Data.Proxy
---import Data.Type.Equality
---import GHC.TypeLits
---import SeeReason.Errors.Sort
+import Data.Type.Equality
+import GHC.TypeLits
+import SeeReason.Errors.Sort
 
-{-
 data MemberTest
   = NotFound ErrorMessage
   | Found
 
+{-
 type family IsJust x where
   IsJust ('Just x) = 'True
   IsJust 'Nothing = 'False
 -}
 
 type family MemberP x ys where
-  MemberP x '[] = 'False
-  MemberP x (x ': ys) = 'True
+  MemberP x '[] = 'NotFound ('Text "Member " ':<>: 'ShowType x ':<>: 'Text " not found in " ':<>: 'Text "ShowType ys")
+  MemberP x (x ': ys) = 'Found
   MemberP x (y ': ys) = MemberP x ys
   MemberP x ys = MemberP x ys
 
 {-
 type family Nub xs where
   Nub '[] = '[]
-  Nub (x ': ys) = If (MemberP x ys == 'True) ys (x ': Nub ys)
+  Nub (x ': ys) = If (MemberP x ys == 'Found) ys (x ': Nub ys)
 -}
 
 type family Delete e xs where
@@ -141,13 +141,13 @@ instance (SafeCopy e, S.Serialize e, Typeable e,  S.Serialize (OneOf s), Typeabl
 
 -- * Put1
 
-class (MemberP e es ~ 'True) => Put1 e es where
+class (MemberP e es ~ 'Found) => Put1 e es where
   put1 :: e -> OneOf es
 
 instance {-# OVERLAPS #-} Put1 e (e ': xs) where
   put1 e = Val e
 
-instance {-# OVERLAPS #-} (MemberP e (f:xs) ~ 'True, Put1 e xs, Delete e (f:xs) ~ (f : Delete e xs)) => Put1 e (f ': xs) where
+instance {-# OVERLAPS #-} (MemberP e (f:xs) ~ 'Found, Put1 e xs, Delete e (f:xs) ~ (f : Delete e xs)) => Put1 e (f ': xs) where
   put1 e = NoVal (put1 e)
 
 throwMember :: forall e (es :: [*]) m a. (Put1 e es, MonadError (OneOf es) m) => e -> m a
@@ -155,7 +155,7 @@ throwMember = throwError . put1
 
 -- * Get1
 
-class (MemberP e es ~ 'True) => Get1 e es where
+class (MemberP e es ~ 'Found) => Get1 e es where
   get1 :: OneOf es -> Maybe e
 
 instance {-# OVERLAPS #-} Get1 e (e ': xs) where
@@ -163,22 +163,21 @@ instance {-# OVERLAPS #-} Get1 e (e ': xs) where
   get1 (NoVal _) = Nothing
   get1 Empty = error "impossible"
 
-instance {-# OVERLAPS #-} (MemberP e (f:xs) ~ 'True, Get1 e xs, Delete e (f:xs) ~ (f : Delete e xs)) => Get1 e (f ': xs) where
+instance {-# OVERLAPS #-} (MemberP e (f:xs) ~ 'Found, Get1 e xs, Delete e (f:xs) ~ (f : Delete e xs)) => Get1 e (f ': xs) where
   get1 (NoVal o) = get1 o
   get1 (Val _e) = Nothing
   get1 Empty = error "impossible"
 
 -- * Remove
 
-class (MemberP e es ~ 'True) => Remove e es where
+class (MemberP e es ~ 'Found) => Remove e es where
   remove :: Proxy e -> OneOf es -> OneOf (Delete e es)
-
 instance {-# OVERLAPS #-} Remove e (e ': xs) where
   remove _ (Val _e) = Empty
   remove _ (NoVal o) = o
   remove _ Empty = Empty
 
-instance {-# OVERLAPS #-} (MemberP e (f:xs) ~ 'True, Remove e xs, Delete e (f:xs) ~ (f : Delete e xs)) => Remove e (f ': xs) where
+instance {-# OVERLAPS #-} (MemberP e (f:xs) ~ 'Found, Remove e xs, Delete e (f:xs) ~ (f : Delete e xs)) => Remove e (f ': xs) where
   remove _p (Val v) = (Val v) -- :: OneOf (f ': (Delete e xs))
   remove p (NoVal o) = NoVal (remove p o)
   remove _p Empty = Empty
