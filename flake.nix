7a8f1df642a9a73c9e24dc7979ce89dff3010d7f
Clifford Beshers <beshers@seereason.com>
2026-01-14 11:03:24 -0800

    update lock

--- flake.nix
+++ flake.nix
@@ -1,13 +1,15 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    userid.url = "github:seereason/userid";
   };
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, userid }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
       overlay = final: prev: {
         sr-errors = final.callCabal2nix "sr-errors" ./. { };
+        # userid = userid;
       };
       myHaskellPackages = pkgs.haskellPackages.extend overlay;
     in
