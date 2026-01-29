d02c4d57f5f4adcf57b4ccdde57db33fc84c50da
Clifford Beshers <beshers@seereason.com>
2026-01-29 10:13:56 -0800

    cleanup the flake a bit

--- flake.nix
+++ flake.nix
@@ -6,14 +6,33 @@
   outputs = { self, nixpkgs, userid }:
     let
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
       overlay = final: prev: {
         sr-errors = final.callCabal2nix "sr-errors" ./. { };
-        # userid = userid;
+        userid = userid;
       };
-      myHaskellPackages = pkgs.haskellPackages.extend overlay;
-    in
-    {
-      packages.${system}.default = myHaskellPackages.sr-errors;
-    };
+      pkgs = nixpkgs.legacyPackages.${system};
+      version="ghc9122";
+    in with pkgs;
+      let ghcPackages = haskell.packages.${version};
+          ghcjsPackages = pkgsCross.ghcjs.haskell.packages.${version};
+          myGHCPackages = ghcPackages.extend overlay;
+          myGHCJSPackages = ghcjsPackages.extend overlay;
+      in
+        {
+          packages.${system}.default = myGHCPackages.sr-errors;
+          devShells.${system}.default = with pkgs;
+            mkShell
+              {
+                packages = [ (ghcPackages.ghcWithPackages (p: (with p; [mtl happstack-server])))
+                             cabal-install cabal2nix
+                             (ghcjsPackages.ghcWithPackages (p: with p; [ghcjs-dom mtl]))
+                           ];
+                shellHook = ''
+            alias ghcjs=javascript-unknown-ghcjs-ghc
+            alias ghcjs-pkg=javascript-unknown-ghcjs-ghc-pkg
+        '';
+                
+              };
+
+        };
 }
