3e243a01d5f7376c08ba2493099afd9f0ed9aaad
Clifford Beshers <beshers@seereason.com>
2026-03-31 10:55:52 -0700

    trying the inclusion of the pkg sets again, just to see what goes wrong

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
-  # inputs.pkgs.url = "git+ssh://git@github.com/seereason/sr-flake?dir=sr-nixpkgs&ref=main";
+  inputs.pkgs.url = "git+ssh://git@github.com/seereason/sr-flake?dir=sr-nixpkgs&ref=main";
   inputs.libs.url = "git+ssh://git@github.com/seereason/sr-flake?dir=sr-libs&ref=main";
 
-  outputs = { self, libs }: {
+  outputs = { self, pkgs, libs }: {
     devShells = libs.devShells;
   };
 }
