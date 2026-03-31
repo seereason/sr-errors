2b27affab174926d066063ca5af9237ef4686474
Clifford Beshers <beshers@seereason.com>
2026-03-31 10:56:35 -0700

    Revert "trying the inclusion of the pkg sets again, just to see what goes wrong"
    This reverts commit 3e243a01d5f7376c08ba2493099afd9f0ed9aaad.
    
    that worked, but I don't think it's a good idea.

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
-  inputs.pkgs.url = "git+ssh://git@github.com/seereason/sr-flake?dir=sr-nixpkgs&ref=main";
+  # inputs.pkgs.url = "git+ssh://git@github.com/seereason/sr-flake?dir=sr-nixpkgs&ref=main";
   inputs.libs.url = "git+ssh://git@github.com/seereason/sr-flake?dir=sr-libs&ref=main";
 
-  outputs = { self, pkgs, libs }: {
+  outputs = { self, libs }: {
     devShells = libs.devShells;
   };
 }
